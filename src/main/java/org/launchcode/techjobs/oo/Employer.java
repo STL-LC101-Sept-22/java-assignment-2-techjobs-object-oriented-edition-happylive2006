98c871cf78613ef71bf4a082dd27da3280fc9f02
Meiling <happylive_2006@hotmail.com>
2023-02-13 15:02:32 -0600

    update

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -24,18 +24,18 @@ public class Employer extends JobField {
 //        return value;
 //    }
 
-    @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
-        if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
-        Employer employer = (Employer) o;
-        return getId() == employer.getId();
-    }
+//    @Override
+//    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+//        if (this == o) return true;
+//        if (!(o instanceof Employer)) return false;
+//        Employer employer = (Employer) o;
+//        return getId() == employer.getId();
+//    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
 
     // Getters and Setters:
 
